1953e2212f1d4ebece195c44e2530efde96260ac
Steve Ellis <sellis@ebsco.com>
2025-10-03 14:28:40 -0400

    Renaming all references to template

--- src/main/java/org/folio/rtaccache/RtacCacheApplication.java
+++ src/main/java/org/folio/rtaccache/RtacCacheApplication.java
@@ -1,13 +1,13 @@
-package org.folio.template;
+package org.folio.rtaccache;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FolioSpringTemplateApplication {
+public class RtacCacheApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(FolioSpringTemplateApplication.class, args);
+    SpringApplication.run(RtacCacheApplication.class, args);
   }
 
 }
--- src/test/java/org/folio/rtaccache/RtacCacheApplicationTest.java
+++ src/test/java/org/folio/rtaccache/RtacCacheApplicationTest.java
@@ -1,4 +1,4 @@
-package org.folio.template;
+package org.folio.rtaccache;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -19,7 +19,7 @@ import org.folio.tenant.rest.resource.TenantApi;
 
 @ActiveProfiles({"test", "testcontainers-pg"})
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class FolioSpringTemplateApplicationTest {
+class RtacCacheApplicationTest {
 
   @EnableAutoConfiguration(exclude = {FolioLiquibaseConfiguration.class})
   @RestController("folioTenantController")
