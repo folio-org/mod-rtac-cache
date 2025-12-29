8136989152c8aa926c5a8bdf922dfc0510bef4ca
Illia Borysenko <Illia_Borysenko@epam.com>
2025-12-29 17:15:56 +0200

    add logs

--- src/main/java/org/folio/rtaccache/service/handler/impl/ItemBoundWithDeleteEventHandler.java
+++ src/main/java/org/folio/rtaccache/service/handler/impl/ItemBoundWithDeleteEventHandler.java
@@ -2,6 +2,7 @@ package org.folio.rtaccache.service.handler.impl;
 
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.folio.rtaccache.domain.RtacHoldingId;
 import org.folio.rtaccache.domain.dto.BoundWithPart;
 import org.folio.rtaccache.domain.dto.InventoryEntityType;
@@ -16,6 +17,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Log4j2
 public class ItemBoundWithDeleteEventHandler implements InventoryEventHandler {
 
   private final RtacHoldingRepository holdingRepository;
@@ -24,14 +26,18 @@ public class ItemBoundWithDeleteEventHandler implements InventoryEventHandler {
   @Override
   @Transactional
   public void handle(InventoryResourceEvent resourceEvent) {
+    log.info("Handling Item BoundWith delete event for resource: {}", resourceEvent);
     var boundWithPart = resourceEventUtil.getNewFromInventoryEvent(resourceEvent, BoundWithPart.class);
     var rtacHoldingId = new RtacHoldingId();
     rtacHoldingId.setId(UUID.fromString(boundWithPart.getItemId()));
     rtacHoldingId.setType(TypeEnum.ITEM);
     rtacHoldingId.setInstanceId(UUID.fromString(boundWithPart.getInstanceId()));
+    log.info("Fetching RTAC holding for BoundWith item with id: {}", rtacHoldingId);
     holdingRepository.findById(rtacHoldingId).ifPresent(existingItemEntity -> {
       var itemRtacHolding = existingItemEntity.getRtacHolding();
+      log.info("Found RTAC holding for BoundWith item: {}", itemRtacHolding);
       if (itemRtacHolding.getIsBoundWith()) {
+        log.info("Deleting RTAC holding for BoundWith item with id: {}", rtacHoldingId);
         holdingRepository.deleteById(rtacHoldingId);
       }
     });
