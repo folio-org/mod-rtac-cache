9028790aa608cdfb84583dd7b8505b570f91b207
izhunke <Ivan_Zhunkivskyi@epam.com>
2026-01-14 16:26:39 +0200

    MODRTACHCE-23: fix transaction

--- src/main/java/org/folio/rtaccache/repository/RtacHoldingRepository.java
+++ src/main/java/org/folio/rtaccache/repository/RtacHoldingRepository.java
@@ -14,6 +14,7 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface RtacHoldingRepository extends JpaRepository<RtacHoldingEntity, RtacHoldingId>, RtacHoldingRepositoryCustom {
@@ -58,6 +59,7 @@ public interface RtacHoldingRepository extends JpaRepository<RtacHoldingEntity, 
 
   void deleteByIdId(UUID id);
 
+  @Transactional
   @Modifying(clearAutomatically = true, flushAutomatically = true)
   void deleteAllByIdInstanceId(UUID instanceId);
 
